1bbca021b2ef065261697731cf13a10a84a686eb
Bianca Bojescu <bianca.bojescu@gmail.com>
2025-05-05 22:26:26 +0300

    timetable

---
+++ TimeTable-frontend/nuxt.config.ts
@@ -0,0 +1,26 @@
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL,
+      apiUrl: process.env.API_URL,
+    },
+  },
+
+  ssr: false,
+
+  imports: {
+    dirs: ["store/.ts", "types/**/.ts", "types/*.ts"],
+  },
+
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  components: [{ path: "~/components", pathPrefix: false }],
+})
---
+++ TimeTable-frontend/types/session.ts
@@ -0,0 +1,10 @@
+export interface Session {
+    time_slot: {
+      id: number
+      day: string
+    }
+    discipline: string
+    group: string
+    teacher: string
+  }
+  
