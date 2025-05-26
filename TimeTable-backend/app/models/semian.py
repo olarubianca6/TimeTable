e0e95cc32306eccef828f2528421fae2533b770b
Bianca Bojescu <bianca.bojescu@gmail.com>
2025-05-26 22:49:32 +0300

    fix

--- TimeTable-backend/app/models/semian.py
+++ TimeTable-backend/app/models/semian.py
@@ -6,7 +6,7 @@ class Semian(db.Model):
     name = db.Column(db.String, nullable=False)
     year_id = db.Column(db.Integer, db.ForeignKey('years.id', ondelete='CASCADE'), nullable=False)
 
-    year = db.relationship('Year', back_populates='semian', passive_deletes=True)
+    year = db.relationship('Year', back_populates='semiani', passive_deletes=True)
     groups = db.relationship('Group', back_populates='semian', cascade='all, delete-orphan', passive_deletes=True)
 
     def __init__(self, **kwargs):
